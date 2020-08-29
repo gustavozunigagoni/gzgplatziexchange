41e737b203727b4783d32c399da6786f69b9bb50
gustavozunigagoni <gustavozunigagoni@yahoo.com>
2020-08-29 11:42:10 -0600

    Final

---
+++ src/router/index.js
@@ -0,0 +1,38 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Error from "../views/Error.vue";
+import CoinDetail from "../views/CoinDetail.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About
+  },
+  {
+    path: "/coin/:id",
+    name: "coin-detail",
+    component: CoinDetail
+  },
+  {
+    path: "*",
+    name: "Error",
+    component: Error
+  }
+];
+
+const router = new VueRouter({
+  mode: "history",
+  routes
+});
+
+export default router;
